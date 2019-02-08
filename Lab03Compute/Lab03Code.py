702986448d1fa3e1476628c2fdf8075d72818f85
Matej Babis <matej@bab.is>
2019-02-08 10:53:28 +0000

    Lab03 complete code

--- Lab03Compute/Lab03Code.py
+++ Lab03Compute/Lab03Code.py
@@ -19,30 +19,40 @@
 
 from petlib.ec import EcGroup
 
+
 def setup():
     """Generates the Cryptosystem Parameters."""
     G = EcGroup(nid=713)
     g = G.hash_to_point(b"g")
     h = G.hash_to_point(b"h")
     o = G.order()
-    return (G, g, h, o)
+    return G, g, h, o
+
 
 def keyGen(params):
    """ Generate a private / public key pair """
    (G, g, h, o) = params
    
-   # ADD CODE HERE
+   priv = G.order().random()
+   pub = priv * g
+   
+   return priv, pub
 
-   return (priv, pub)
 
 def encrypt(params, pub, m):
     """ Encrypt a message under the public key """
     if not -100 < m < 100:
         raise Exception("Message value to low or high.")
 
-   # ADD CODE HERE
+    (G, g, h, o) = params
+    k = G.order().random()      # random number mod the order of the group
+    
+    a = k * g                   # g^k
+    b = k * pub + m * h         # g^xk h^m
 
-    return c
+    # return ciphertext pair
+    return a, b
+
 
 def isCiphertext(params, ciphertext):
     """ Check a ciphertext """
@@ -52,6 +62,7 @@ def isCiphertext(params, ciphertext):
     ret &= G.check_point(a)
     ret &= G.check_point(b)
     return ret
+
 
 _logh = None
 def logh(params, hm):
@@ -70,19 +81,22 @@ def logh(params, hm):
 
     return _logh[hm]
 
+
 def decrypt(params, priv, ciphertext):
     """ Decrypt a message using the private key """
     assert isCiphertext(params, ciphertext)
-    a , b = ciphertext
 
-   # ADD CODE HERE
+    a, b = ciphertext
+
+    hm = b - priv * a       # inner part of the log: b * (a^x)^(-1)
 
     return logh(params, hm)
+
 
 #####################################################
 # TASK 2 -- Define homomorphic addition and
 #           multiplication with a public value
-# 
+#
 
 def add(params, pub, c1, c2):
     """ Given two ciphertexts compute the ciphertext of the 
@@ -91,18 +105,23 @@ def add(params, pub, c1, c2):
     assert isCiphertext(params, c1)
     assert isCiphertext(params, c2)
 
-   # ADD CODE HERE
+    a1, b1 = c1
+    a2, b2 = c2
+    
+    # E(m1 + m2, k1 + k2) = (a1a2, b1b2)
+    return a1 + a2, b1 + b2
 
-    return c3
 
 def mul(params, pub, c1, alpha):
     """ Given a ciphertext compute the ciphertext of the 
         product of the plaintext time alpha """
     assert isCiphertext(params, c1)
 
-   # ADD CODE HERE
+    a1, b1 = c1
 
-    return c3
+    # E(cm1, ck1) = (a1^c, b1^c)
+    return alpha * a1, alpha * b1
+
 
 #####################################################
 # TASK 3 -- Define Group key derivation & Threshold
@@ -111,23 +130,30 @@ def mul(params, pub, c1, alpha):
 
 def groupKey(params, pubKeys=[]):
     """ Generate a group public key from a list of public keys """
-    (G, g, h, o) = params
-
-   # ADD CODE HERE
+    
+    assert(len(pubKeys) > 0)    # sanity check
+    
+    # pk = g^(x1+...xn)
+    pub = pubKeys[0]
+    for pk in pubKeys[1:]:
+        pub += pk
 
     return pub
+
 
 def partialDecrypt(params, priv, ciphertext, final=False):
     """ Given a ciphertext and a private key, perform partial decryption. 
         If final is True, then return the plaintext. """
     assert isCiphertext(params, ciphertext)
     
-    # ADD CODE HERE
+    a1, b = ciphertext      # a1 == a
+    b1 = b - priv * a1      # intermediate step: b * (a^x)^(-1)
 
     if final:
         return logh(params, b1)
     else:
         return a1, b1
+
 
 #####################################################
 # TASK 4 -- Actively corrupt final authority, derives
@@ -141,10 +167,17 @@ def corruptPubKey(params, priv, OtherPubKeys=[]):
         public key corresponding to a private key known to the
         corrupt authority. """
     (G, g, h, o) = params
-    
-   # ADD CODE HERE
 
-    return pub
+    assert (len(OtherPubKeys) > 0)  # sanity check
+
+    # derive the public key of the malicious authority
+    malicious_pub = priv * g
+    # compute the legitimate public key
+    legit_pub = groupKey(params, OtherPubKeys)
+
+    # return corrupt public key: malicious_pk / product of legitimate_pks
+    return malicious_pub - legit_pub
+
 
 #####################################################
 # TASK 5 -- Implement operations to support a simple
@@ -157,18 +190,29 @@ def encode_vote(params, pub, vote):
         zero and the votes for one."""
     assert vote in [0, 1]
 
-   # ADD CODE HERE
+    # if the vote is 0, have v0 = 1 (and v1 = 0)
+    # do the opposite for when vote is 1
+    v0 = encrypt(params, pub, 1-vote)
+    v1 = encrypt(params, pub, vote)
 
-    return (v0, v1)
+    return v0, v1
+
 
 def process_votes(params, pub, encrypted_votes):
     """ Given a list of encrypted votes tally them
         to sum votes for zeros and votes for ones. """
     assert isinstance(encrypted_votes, list)
+    assert (len(encrypted_votes) > 0)  # sanity check
     
-   # ADD CODE HERE
+    # starting value
+    total_v0, total_v1 = encrypted_votes[0]
+    # accumulate total for each vote (without decrypting)
+    for v0, v1 in encrypted_votes[1:]:
+        total_v0 = add(params, pub, v0, total_v0)
+        total_v1 = add(params, pub, v1, total_v1)
 
-    return tv0, tv1
+    return total_v0, total_v1
+
 
 def simulate_poll(votes):
     """ Simulates the full process of encrypting votes,
